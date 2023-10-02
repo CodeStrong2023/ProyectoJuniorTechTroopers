509c95cc8cef903c81d2a2606a9038e2dafb7dd8
joaquin9830 <joaquin9811@gmail.com>
2023-10-02 17:37:28 -0300

    Conexión a la base de datos | librería MySQL

--- TROOPERSSTAY/src/PROYECTO/MAIN.java
+++ TROOPERSSTAY/src/PROYECTO/MAIN.java
@@ -1,8 +1,19 @@
 
 package PROYECTO;
 
+import databases.conexion;
+
 public class MAIN {
     public static void main(String[] args) {
         
+        conexion cn = new conexion();
+       
+       
+        if(cn.conexion != null){
+            System.out.println("La conexión fue exitosa");
+            
+        }else{
+            System.out.println("La conexión falló");
+        }
     }
 }
--- TROOPERSSTAY/src/databases/conexion.java
+++ TROOPERSSTAY/src/databases/conexion.java
@@ -1,13 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package databases;
 
-/**
- *
- * @author nelso
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class conexion {
-    
+     public Connection conexion;
+    //Creamos el metodo conexion para conectarnos con la base de datos "TrooperStay"
+    public conexion(){
+        try{
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/trooper_stay","root","32893064");
+        }catch (Exception e){
+            //En caso de que ocurra una excepción durante la conexión, se muestra un mensaje de error
+            System.err.print("Error" + e);
+        }
+        
+    }
 }
