a21fece1dcd08dba15de45836daf2f94d00fbdb8
joaquin9830 <joaquin9811@gmail.com>
2023-10-09 19:45:15 -0300

    Conexion a la base de datos y verificación de la misma

--- TrooperStayJava/src/main/java/database/Conexion.java
+++ TrooperStayJava/src/main/java/database/Conexion.java
@@ -1,4 +1,22 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Conexion {
+    
+    public Connection conexion;
+    //Creamos el metodo conexion para conectarnos con la base de datos TrooperStay
+    public Conexion() {
+        try {
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/trooper_stay", "root", "02091898");
+        } catch (Exception e) {
+            //En caso de que ocura una excepción durante la conexión, se muestra un mensaje de erorr
+            System.err.print("Error" + e);
+        }
+    }
+    //Getter
+    public Connection getConexion() {
+        return conexion;
+    }
 }
--- TrooperStayJava/src/main/java/main.java
+++ TrooperStayJava/src/main/java/main.java
@@ -1,12 +1,16 @@
+
+import database.Conexion;
+
 public class main {
     public static void main(String[] args) {
         //Creamos una instancia de la clase conexion para establecer la conexión a la base de datos
-        conexion cn = new conexion();
+        Conexion cn = new Conexion();
         //Intanciamos la clase Menu
-        Menu menu = new Menu();
+       
+        //Verificamos si la conexión a la base de datos es exitosa
         if(cn.conexion != null){
             System.out.println("La conexión fue exitosa");
-            menu.Menu();
+            
         }else{
             System.out.println("La conexión falló");
         }
