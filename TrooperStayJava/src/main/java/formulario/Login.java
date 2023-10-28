5fde8c84fe7bd9fee9c6ade0679601d560a3bf5c
Valentino <jostar843@gmail.com>
2023-10-28 19:38:01 -0300

    Menu y logueo

--- TrooperStayJava/src/main/java/formulario/Login.java
+++ TrooperStayJava/src/main/java/formulario/Login.java
@@ -1,4 +1,34 @@
+
 package formulario;
 
+import database.Conexion;
+import database.ObtenerInformacion;
+import modelo.Usuario;
+
+import javax.swing.JOptionPane;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+
+import java.sql.SQLException;
+
 public class Login {
+
+    ObtenerInformacion obtenerInformacion = new ObtenerInformacion();
+
+    //Creamos una instancia de la clase panel
+    Panel panel = new Panel();
+
+    public void iniciarSesion() {
+        String nombreUsuario = JOptionPane.showInputDialog("Ingrese su nombre de usuario:");
+        String contrasenia = JOptionPane.showInputDialog("Ingrese su contraseña:");
+
+        Usuario usuario = obtenerInformacion.loguearse(nombreUsuario, contrasenia);
+
+        if (usuario != null){
+            panel.Panel(usuario);
+        }
+
+    }
 }
--- TrooperStayJava/src/main/java/formulario/Menu.java
+++ TrooperStayJava/src/main/java/formulario/Menu.java
@@ -1,4 +1,41 @@
 package formulario;
 
+import javax.swing.JOptionPane;
+
 public class Menu {
+
+    boolean salir = false;
+    Login lo = new Login();
+    Registro re = new Registro();
+
+
+    
+   
+
+    public void Menu() {
+        while (!salir) {
+            //Creamos un arreglo que contenga los elementos del panel de opciones
+            String [] arreglo = {"Iniciar Sesión", "Registrarte", "Salir"};
+            // usamos el comando de JOptionPane.showOptionDiaglog te permite seleeccional opciones
+            int opcion = JOptionPane.showOptionDialog(null, "Elige una opción", "MENU PRINCIPAL", 0, JOptionPane.QUESTION_MESSAGE, null, arreglo, "");
+            
+            //Por medio de un SWITCH derivamos la opcion seleccionada en base a la posicion del arreglo
+            switch (opcion) {
+                case 0:
+                    // Lógica para iniciar sesión
+                    lo.iniciarSesion();
+                    break;
+                case 1:
+                    // Lógica para registrarse
+                    re.registrarUsuario();
+                    break;
+                default:
+                    // Cualquier otra opcion sale
+                    salir = true;
+                    break;
+            }
+        }
+    }
+
+  
 }
